d009181e5dbab52e49ae0e6d5fc6a7789edc64f1
VickyMontanezCampus <vmontanez707@gmail.com>
2023-06-07 09:29:25 -0500

    feat: :sparkles: Add new documentation and notes about a standar method of SINGLENTON
    Add notes and documentation about the standar structure of SINGLENTON

--- 06_07_2023/singleton.php
+++ 06_07_2023/singleton.php
@@ -1,48 +1,61 @@
 <?php
-    //* declare(strict) verifica estrictamente los tipos de datos  que entran en funciones y métodos para que coincidan.
-    declare(strict_types=1);
+    //! STANDAR STRUCTURE OF SINGLETON
 
-    class Humano{
-
-        //* Se hace una variable estatica para Instanciar
-        //? Una instancia es la representación de la clase
-
+    //* trait =  es un método que permite reutilizar código funcional sin necesidad de escribir el mismo código una y otra vez.
+    trait getInstance{
+        //* se declara la variable estática "$intance"
         static $instance;
-        public $name;
-        private $age;
-
-        function __construct($name, int $age){
-            $this->name = $name;
-            $this->age = $age;
-        }
-
-        //! Se coloca otra función estatica debajo del constructor con el mismo nombre de la variable
-
+        //* se declara la función estática
         static function getInstance(){
             $arg = func_get_args();
             $arg = array_pop($arg);
-            //* self se utiliza para acceder a todos los métodos y variables de una clase sin tener que instanciarla manualmente
-
-            //? SI(la variable $instance de la clase es NUll), entonces, Defina la varianble con una INSTANCIA
             if (self::$instance == null) {
-                // self::$instance = new Humano("Vickysiiiiitaaa", 22 );
-
-                //? SI(la variable $instance de la clase es NUll), Definala como una instancia = "new self", desectructure sus argumentos "...", paselos como array "(array)" y devuelva el último "array_pop" elemento del array "func_get_args"
                 self::$instance = new self(...(array) $arg);
-
             }
+            //* se retorna la variable estática
             return self::$instance;
         }
+    }
 
+    //* Se declara la primer clase "Humano"
+    class Humano{
+
+        //* Se declara la función Construtor con las variables como parametros ya declaradas como privadas o públicas
+        function __construct(private $age, public $name, public $cellphone){}
+
+        //* se coloca el método use para utilizar el trait = USE NombreDelTrait
+        use getInstance;
+
+        //*se utiliza la función que se quiera utilizar en la clase
+        public function getDatos(){
+            $data = [
+                $this->name,
+                $this->age,
+                $this->cellphone
+            ];
+            return $data;
+        }
+    }
+
+
+
+
+    //* Se declara la segunda clase "Perro"
+    class Macotas{
+
+        //* Se declara la función Construtor con la variables como parametro ya declarada como privada
+        function __construct(private $name){}
+
+        //* se coloca el método use para utilizar el trait = USE NombreDelTrait
+        use getInstance;
+
+        //*se utiliza la función que se quiera utilizar en la clase
         public function getName(){
             return $this->name;
         }
-    
     }
-
-    //! Para acceder a un método, PRIMERO se debe acceder al método estático de la Instancia y LUEGO " -> " entra al método
     
-    // var_dump(Humano::getInstance()->getName());
-
-    var_dump(Humano::getInstance(["name"=>"Vickysitaaa Vanesssaa", "age"=> 17])-> getName());
+    //* NombreDeLaClase :: NombreDelTrait( $SePasanLosParametros = declarados )-> seAccedeALaFunción()
+    var_dump(Humano::getInstance(["name"=>"Vicky Vanessa Montañez Molina","age"=>17, "cellphone"=> 3224543241])->getDatos());
+    var_dump(Macotas::getInstance(["Gatoto, Pachito, Colanta, Morita, Sarnita"])->getName());
 ?>
